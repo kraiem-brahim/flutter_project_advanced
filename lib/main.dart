15ee59b639c5cfa6f688e7c4a322371a28b00998
MSI\Kraiem <krbra98@gmail.com>
2024-02-12 11:54:12 +0100

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter advanced',
+      title: 'Flutter advanced course',
       theme: ThemeData(
         // This is the theme of your application.
         //
